fc23366b66b6563610cb6073d17e48824fab2c2f
AmineAyouch <amineayouch4@gmail.com>
2026-02-16 15:00:02 +0000

    welcom

---
+++ firstprodart/bin/firstprodart.dart
@@ -0,0 +1,5 @@
+import 'package:firstprodart/firstprodart.dart' as firstprodart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${firstprodart.calculate()}!');
+}
---
+++ firstprodart/test/firstprodart_test.dart
@@ -0,0 +1,8 @@
+import 'package:firstprodart/firstprodart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
